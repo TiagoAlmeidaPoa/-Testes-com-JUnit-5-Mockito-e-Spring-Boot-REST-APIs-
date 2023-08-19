a76d2bae5d18102e1b6114d9bb298aceeed2678f
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-08-19 18:47:16 -0300

    feat: Created Entity User

--- src/main/java/br/com/almeidatiago/api/ApiApplication.java
+++ src/main/java/br/com/almeidatiago/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.almeidatiago.api;
 
+import br.com.almeidatiago.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/br/com/almeidatiago/api/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.almeidatiago.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
