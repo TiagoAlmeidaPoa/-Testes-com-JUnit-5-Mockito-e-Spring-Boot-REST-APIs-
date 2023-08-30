07701c09bb93a17083b6692646e68b766e77c818
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-08-30 18:35:54 -0300

    feat: created endpoint create for UserEntity

--- src/main/java/br/com/almeidatiago/api/services/UserService.java
+++ src/main/java/br/com/almeidatiago/api/services/UserService.java
@@ -1,10 +1,12 @@
 package br.com.almeidatiago.api.services;
 
 import br.com.almeidatiago.api.domain.UserEntity;
+import br.com.almeidatiago.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     UserEntity findById(Integer id);
     List<UserEntity> findAll();
+    UserEntity create(UserDTO obj);
 }
